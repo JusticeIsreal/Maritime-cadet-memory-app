0ce7827e58ac148b5e672be40360ccf379093ca7
Justice Isreal <justiceyba@gmail.com>
2023-08-26 22:47:48 +0100

    change

--- Components/Profile.tsx
+++ Components/Profile.tsx
@@ -1,33 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
-import { RxAvatar } from "react-icons/rx";
-import { SlCallEnd } from "react-icons/sl";
-import { BsWhatsapp } from "react-icons/bs";
-import { AiOutlineMail } from "react-icons/ai";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../Firebase";
 import Image from "next/image";
-// import Loader from "../../../Components/Loader";
+
 function CustomerID() {
   // user session
   const { data: session } = useSession();
-  const [pictures, setPictures] = useState<any[]>([]);
+  const [allUsers, setAllUsers] = useState<any[]>([]);
   useEffect(() => {
     return onSnapshot(
       query(collection(db, "registered_Users"), orderBy("time", "desc")),
       (snapshot) => {
-        setPictures(snapshot.docs);
+        setAllUsers(snapshot.docs);
       }
     );
   }, [session]);
 
-  // const openPicture = pictures.map((pic) => pic.data());
-  const openPictures = pictures.map((pic) => pic.data());
+  // filter user from users array
+  const openPictures = allUsers.map((pic) => pic.data());
   const userposts = openPictures.filter(
     (post) => post?.userId === (session?.user as { uid: any })?.uid
   );
-  console.log(userposts);
+
   return (
     <div className="singleuser-con">
       <div className="avatar">
@@ -36,21 +32,26 @@ function CustomerID() {
             className="imgQ"
             src={userposts[0]?.image}
             alt="img"
-            // fill
-            // sizes="100vw"
             width={50}
             height={50}
           />
         </div>
         <div className="user-list">
-          <h3>{userposts[0]?.name}</h3>
+          {userposts[0]?.name.split(" ").length > 1 ? (
+            <h3>
+              {userposts[0]?.name.split(" ")[0]}{" "}
+              {userposts[0]?.name.split(" ")[1]}
+            </h3>
+          ) : (
+            <h3>{userposts[0]?.name.split(" ")[0]}</h3>
+          )}
+
           <p>{userposts[0]?.email}</p>
           <p>{userposts[0]?.phone_number}</p>
           <p>
             {userposts[0]?.startyear} - {userposts[0]?.endyear}
           </p>
           <p>{userposts[0]?.department}</p>
-          {/* <p>{userposts[0]?.position_held}CCC</p> */}
         </div>
       </div>
     </div>
